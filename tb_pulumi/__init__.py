e2fe8e001f13a4a87eb81d64a70c88c9f911b7db
Ryan Jung <rjung@thunderbird.net>
2025-04-30 09:43:10 -0600

    Remove pulumi_last_run_by tag (#154)

--- tb_pulumi/__init__.py
+++ tb_pulumi/__init__.py
@@ -8,8 +8,7 @@ import pulumi
 import yaml
 
 from functools import cached_property
-from os import environ, getlogin, path
-from socket import gethostname
+from os import environ
 from tb_pulumi.constants import DEFAULT_PROTECTED_STACKS
 from typing import Any
 
@@ -46,24 +45,9 @@ class ThunderbirdPulumiProject:
         #: Pulumi Resource objects managed by this project
         self.resources: dict = {}
 
-        # Some machines can't run a getlogin(), which is the preferred method, but we support some others
-        try:
-            username = getlogin()
-        except OSError:
-            homepath = path.expanduser('~')
-            unix_style = homepath.split('/')
-            win_style = homepath.split('\\')
-            if len(unix_style) > 1:
-                username = unix_style[-1]
-            elif len(win_style) > 1:
-                username = win_style[-1]
-            else:
-                username = 'unknown'
-
         self.common_tags: dict = {  #: Tags to apply to all taggable resources
             'environment': self.stack,
             'project': self.project,
-            'pulumi_last_run_by': f'{username}@{gethostname()}',
             'pulumi_project': self.project,
             'pulumi_stack': self.stack,
         }
