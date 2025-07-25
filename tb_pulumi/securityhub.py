f080aab995eaaa3a9d1f859eef61832084122f8b
Arron Atchison <arron.atchison@gmail.com>
2025-07-25 15:43:45 -0700

    MISSING TWO UNDERSCORES LOL

--- tb_pulumi/securityhub.py
+++ tb_pulumi/securityhub.py
@@ -5,6 +5,7 @@ import pulumi
 import pulumi_aws as aws
 import tb_pulumi
 
+
 class SecurityHubAccount(tb_pulumi.ThunderbirdComponentResource):
     """**Pulumi Type:** ``tb:securityhub:SecurityHubAccount``
     Enable and Configure AWS SecurityHub for an account/region.
@@ -13,7 +14,7 @@ class SecurityHubAccount(tb_pulumi.ThunderbirdComponentResource):
 
     """
 
-    def __init(
+    def __init__(
         self,
         name: str,
         project: tb_pulumi.ThunderbirdPulumiProject,
@@ -36,8 +37,6 @@ class SecurityHubAccount(tb_pulumi.ThunderbirdComponentResource):
             tags=tags,
         )
 
-        security_hub_account = aws.securityhub.Account(
-            f'{name}', opts=pulumi.ResourceOptions(region)
-        )
+        security_hub_account = aws.securityhub.Account(f'{name}', opts=pulumi.ResourceOptions(region))
 
         self.finish(resources={'security_hub_account': security_hub_account})
